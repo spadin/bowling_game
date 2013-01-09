b2a5824a0cecb4f8bac6b850e452c513bb773789
Sandro Padin <sandropadin@gmail.com>
2013-01-09 15:49:43 -0600

    Second test. Rolling all ones.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,5 +9,13 @@ module Bowling
       end
       game.score.should be 0
     end
+
+    it "calculates score after rolling all ones" do
+      game = Game.new
+      20.times do
+        game.roll 1
+      end
+      game.score.should be 20
+    end
   end
 end
