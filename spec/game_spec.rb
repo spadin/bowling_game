37db361ec55ddf183773e138ee5393d14527b806
Sandro Padin <sandropadin@gmail.com>
2013-01-09 15:50:19 -0600

    Fixing whitespace.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,7 @@ module Bowling
   describe Game do
     it "calculates a gutter game" do
       game = Game.new
-      20.times do 
+      20.times do
         game.roll 0
       end
       game.score.should be 0
