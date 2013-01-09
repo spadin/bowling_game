c1868427e105bd12ec87ab3cf9ef55cfc15a0b75
Sandro Padin <sandropadin@gmail.com>
2013-01-09 15:54:35 -0600

    Refactoring after second test.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,19 +2,21 @@ require_relative "../lib/game"
 
 module Bowling
   describe Game do
-    it "calculates a gutter game" do
-      game = Game.new
-      20.times do
-        game.roll 0
+    let (:game) { Game.new }
+
+    def roll_many(number_of_times, pins)
+      number_of_times.times do
+        game.roll pins
       end
+    end
+
+    it "calculates a gutter game" do
+      roll_many(20, 0)
       game.score.should be 0
     end
 
     it "calculates score after rolling all ones" do
-      game = Game.new
-      20.times do
-        game.roll 1
-      end
+      roll_many(20, 1)
       game.score.should be 20
     end
   end
